88eeb85755f5633ce1dd5860ec88495e30f2c7fc
Diego-PC\Diego <dfhincapiem@unal.edu.co>
2017-02-10 19:11:42 -0500

    no message

---
+++ app/models/messages.rb
@@ -0,0 +1,10 @@
+class Message < ApplicationRecord
+     
+	validates: :title :description, presence: true
+	validates: :description, length: {minimum:10}
+	validates: :title, uniqueness: true
+
+
+
+
+end 
